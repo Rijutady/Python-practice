a475a90bada6f6be6986c138e4e63ee2cf066d1b
Rijutady <rijutady@gmail.com>
2016-10-23 19:58:48 -0400

    Update python loop practice.py

--- python loop practice.py
+++ python loop practice.py
@@ -3,3 +3,17 @@ my_list = [1,9,3,8,5,7]
 for number in my_list:
     print 2*number
     
+    
+    
+    
+    
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+for number in start_list:
+    print square_list.append(number**2)
+
+square_list.sort()
+    
+print square_list
+    
